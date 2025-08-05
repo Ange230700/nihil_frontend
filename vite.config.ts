ae785782da8d0032aee82807a49865e081afbad9
Ange230700 <kouakouangeericstephane@gmail.com>
2025-08-05 05:46:27 +0200

    chore: do stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@nihil_frontend": resolve(__dirname, "src"),
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
