99d4035c0d99156f4517c08c2934b0c4aa3280a2
Ange230700 <kouakouangeericstephane@gmail.com>
2025-08-05 05:35:29 +0200

    chore: do stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 // vite.config.ts
 
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   test: {
-    environment: "jsdom", // so React DOM rendering works
-    globals: true, // so you can use describe/it without imports
-    setupFiles: [], // you can add setup files later if you want
+    environment: "jsdom",
+    globals: true,
+    setupFiles: [],
     coverage: {
-      // optional, but nice to have
       reporter: ["text", "html"],
     },
   },
